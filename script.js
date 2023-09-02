84815aa9bd89c0c17db8822ef05d02a2be0aa812
GOKILA10 <143244926+GOKILA10@users.noreply.github.com>
2023-09-03 01:52:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var a = 10;
+var b = 4;
+c =a+b;
+console.log(c);
+
+var str= "guvi geek";
+console.log(str);
+
+//OBJECT:
+
+var studentDetails={
+    fname:"gokila",
+    lname:"muruganantham",
+    age:25,
+    mail:"goki@gmail.com",
+    contact:98765432,
+    regno: 34545
+
+}
+console.log(studentDetails);
